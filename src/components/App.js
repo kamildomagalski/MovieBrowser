1f576ab7162ba3681202baae6703cd74179a2ddf
Kamil Domagalski <kdbrasi360@gmail.com>
2020-10-14 16:20:44 +0200

    added routing using reactRouter and ReactRouterBootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from "./Header";
-
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './Home'
+import SearchMovie from "./SearchMovie"
+import Contact from "./Contact";
 
 function App() {
   return (
     <>
-      <Header/>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route exact path={'/'}><Home/></Route>
+          <Route exact path={'/search'}><SearchMovie/></Route>
+          <Route exact path={'/contact'}><Contact/></Route>
+        </Switch>
+      </Router>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
 import React, {useState} from 'react';
 import {Container, Navbar, NavLink} from 'react-bootstrap'
 import NavbarCollapse from "react-bootstrap/NavbarCollapse";
+import {BrowserRouter as Router} from "react-router-dom";
+import {LinkContainer} from 'react-router-bootstrap'
 
 function Header() {
   
   return (
     <section>
       <Navbar expand={'lg'} bg={'light'} variant={'light'}>
-      <Navbar.Brand href={'#home'}>Movie Browser</Navbar.Brand>
+        <Navbar.Brand href={'#'}>Movie Browser</Navbar.Brand>
         <NavbarCollapse className={'mr-auto'}>
-          <NavLink>Home</NavLink>
-          <NavLink>Search for movie</NavLink>
-          <NavLink>Contact</NavLink>
+            <LinkContainer to={'/'}>
+              <NavLink>Home</NavLink>
+            </LinkContainer>
+            <LinkContainer to={'/search'}>
+              <NavLink>Search for movie</NavLink>
+            </LinkContainer>
+            <LinkContainer to={'/contact'}>
+              <NavLink>Contact</NavLink>
+            </LinkContainer>
         </NavbarCollapse>
       </Navbar>
     </section>
