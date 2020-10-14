04513be5e876a8660b58c878487fea0704761aad
Kamil Domagalski <kdbrasi360@gmail.com>
2020-10-14 18:17:56 +0200

    added handlers on change and on submit of the form

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,25 +1,49 @@
-import React from 'react';
-import {Container, InputGroup, FormControl, Button} from "react-bootstrap";
+import React, {useState} from 'react';
+import {
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Form
+} from "react-bootstrap";
 
 
 function SearchMovie() {
- return (
-  <section>
-   <Container>
-     <InputGroup className={'mb-3'}>
-       <InputGroup.Prepend>
-         <InputGroup.Text id={'search-addon'}>Enter title</InputGroup.Text>
-       </InputGroup.Prepend>
-       <FormControl
-         placeholder={'e.g. Spiderman'}
-         aria-label={'title'}
-       />
-       <InputGroup.Append>
-         <Button variant={'outline-secondary'}>Search</Button>
-       </InputGroup.Append>
-     </InputGroup>
-   </Container>
-  </section>
- );
+  const [searchTerm, setSearchTerm] = useState('')
+  const [movies, setMovies]= useState([])
+  
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(searchTerm);
+  }
+  
+ 
+  return (
+    <section>
+      <Container>
+        <Form onSubmit={handleSubmit}>
+          <InputGroup className={'mb-3'}>
+            <InputGroup.Prepend>
+              <InputGroup.Text id={'search-addon'}>Enter title</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              value={searchTerm}
+              onChange={handleChange}
+              type={'text'}
+              placeholder={'e.g. Spiderman'}
+              aria-label={'title'}
+            />
+            <InputGroup.Append>
+              <Button variant={'outline-secondary'} type={'submit'}>Search</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form>
+      </Container>
+    </section>
+  );
 }
+
 export default SearchMovie;
