4aa11ca53863c9c2bbf65352fe2df61e74e70385
Kamil Domagalski <kdbrasi360@gmail.com>
2020-10-14 16:06:30 +0200

    add Home, Search and Contact pages

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+function Home() {
+ return (
+  <Container>
+   Home
+  </Container>
+ );
+}
+export default Home;
---
+++ src/components/SearchMovie.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+
+function SearchMovie() {
+ return (
+  <Container>
+   Search
+  </Container>
+ );
+}
+export default SearchMovie;
